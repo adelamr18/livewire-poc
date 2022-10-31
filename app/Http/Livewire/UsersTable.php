1a27f126e89f8cf8285819df00997da3d29f6ad5
a.gebril <a.gebril@metro-markets.de>
2022-10-31 03:32:35 +0100

    Add livewire poc

---
+++ app/Http/Livewire/UsersTable.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\User;
+
+class UsersTable extends Component
+{
+    use WithPagination;
+    public $search = '';
+    public $perPage = 10;
+    public $sortField = 'id';
+    public $sortAsc = true;
+    public $selected = [];
+
+    public function render()
+    {  return view('livewire.users-table', [
+        'users' => User::search($this->search)
+            ->orderBy($this->sortField, $this->sortAsc ? 'asc' : 'desc')
+            ->simplePaginate($this->perPage),
+    ]);
+    }
+
+    public function deleteUsers(){
+     User::destroy($this->selected);
+    }
+}
+
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        User::factory()->times(50)->state([
+            'created_at'=> fn () => now()->subMinutes(rand(0,59)),
+        ])->create();
+    }
+}
